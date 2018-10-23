8ee62b1e137fee914e22874a042fab871e735d73
Christopher Chizinski <chris.chizinski@gmail.com>
2018-10-23 14:00:41 -0500

    modifications to plot and order of code

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,10 +1,14 @@
 
 library(tidyverse)
 library(ggplot2)
-fish <- read_csv("~/R Class Fall 2018/fish.csv")
-View(fish)
+
 source("R/themes.R")
-fish<-read.csv("C:/Users/Sarah/Desktop/gittutorial/data/fish.csv")
+
+fish <- read_csv("data/fish.csv")
+
+View(fish)
+
+# fish<-read.csv("C:/Users/Sarah/Desktop/gittutorial/data/fish.csv")
 
 fish %>% 
   mutate(length_cat=case_when(length>=300~"big", length<300~"small"))->fish
@@ -14,3 +18,14 @@ ggplot()+
   geom_histogram(data=fish, aes(scalelength, fill=length_cat))+ 
   facet_wrap(~length_cat)+ 
   theme_paper()
+
+
+## Chris's modifications
+
+ggplot() +
+  geom_histogram(data = fish, aes(x = scalelength, fill = length_cat), colour = "black", bins = 25) +
+  facet_wrap(~length_cat) +
+  coord_cartesian(ylim = c(0,500), xlim = c(0, 12), expand = FALSE) +
+  theme_classic() +
+  theme(legend.position = "none",
+        panel.spacing = unit(2, "lines"))
